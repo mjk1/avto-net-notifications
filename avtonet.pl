8b4fe12d83924d60888d330d2a9681c01757e35b
mulaz <dmulac@gmail.com>
2014-07-15 11:39:56 +0200

    initial commit

---
+++ avtonet.pl
@@ -0,0 +1,286 @@
+#!/usr/bin/perl 
+#mulaz
+
+use Data::Dumper;
+use LWP::Simple;
+use utf8;
+use open ':std', ':encoding(UTF-8)';
+binmode STDOUT, ':encoding(UTF-8)';
+$debug=0;
+use MIME::Lite;
+
+open FILE, "<", "last.id" or die $!;
+$last=<FILE>;
+close FILE;
+#print $last;
+
+
+@ids=gettop100();
+
+
+foreach(@ids) {
+    $id=$_;
+
+    if($id<=$last) {
+	next;
+    }
+
+
+#    print "$_ \n";
+
+    eval {%cardata=getcarinfo($id); };
+    if($@) {
+	print "error $@\n";
+	print "oglas http://avto.net/_AVTO/ad.asp?ID=$id \n";
+	next;
+    }
+
+    
+    open FILE, ">", "last.id";
+    print FILE $id;
+    close FILE;
+
+
+
+#daj pogoje sem: (<ID> je anonimiziran)
+
+# $VAR1 = {
+#           'ID' => <ID>,
+#           'Letnik' => 2008,
+#           'Stevilo vrat' => '5 vr.',
+#           'Kraj ogleda' => '<a  href="ad.asp?ID=<ID>&show=3" target="_self"><b>Maribor, naslov1</b></a>',
+#           'Image1html' => '<img src="http://images.avto.net/<ID>/1.jpg">',
+#           'Motor (gorivo)' => 'diesel motor',
+#           'Image1' => 'http://images.avto.net/<ID>/1.jpg',
+#           'Tehnicni pregled' => '6/2015',
+#           'Notranjost' => 'temno siva / blago',
+#           'Motor (moc)' => '78 kW        (106 KM)',
+#           'URL' => 'http://avto.net/_AVTO/ad.asp?ID=<ID>',
+#           'Menjalnik' => 'rocni menjalnik (6 pr.)',
+#           'Ohranjenost' => 'odlicno ohranjeno',
+#           'Oblika karoserije' => 'kombilimuzina / hatchback',
+#           'Starost vozila' => 'rabljeno',
+#           'Barva vozila' => 'srebrna metalik',
+#           'Motor (prostornina)' => '1461 ccm',
+#           'Cena' => 4499,
+#           'Ime' => 'Renault Megane 1.5 dCi ',
+#           'Prevozeni km' => '158400',
+#           'Lokacija' => '02'
+#         };
+
+
+
+
+
+    if($cardata{Cena}<5101) {
+        if($cardata{Letnik}>2006) {
+            sendemail(\%cardata,"USER\@gmail.com");
+            print Dumper(\%cardata); #debug
+        }
+    }
+
+
+
+   
+}
+
+
+
+
+
+
+sub getcarinfo {
+    my $car1id=shift;
+
+    my $car1url="http://avto.net/_AVTO/ad.asp?ID=$car1id";
+#    print $car1url ."\n";
+    my $carhtml=get $car1url;
+#    print $carhtml;
+    my %cardata;
+
+    $cardata{URL}=$car1url;
+    $cardata{ID}=$car1id;
+
+
+    if($carhtml=~/<p class="AdDataCenaTOP" >\n*\s*?(.*?)\n*\s*<\/p>/) {
+	print "matcha cena $1\n" if $debug;
+	$cena=$1;
+	chomp $cena;
+	$cena=~s/\D//g;
+#	print "cena: $cena\n";
+	$cardata{Cena}=$cena;
+    }
+    else {
+	die("ni cene");
+    }
+
+
+
+    if($carhtml=~/<title>(.*)<\/title>/) {
+	print "matcha title $1 \n" if $debug;
+	$ime=$1;
+	$ime=~s/ :: www.Avto.net :://g;
+	chomp($ime);
+#	print "ime: $ime\n";
+        $ime=~s/č/c/g;
+        $ime=~s/Č/C/g;
+        $ime=~s/Š/S/g;
+        $ime=~s/š/s/g;
+        $ime=~s/Ž/Z/g;
+        $ime=~s/ž/z/g;
+
+
+
+	$cardata{Ime}=$ime;
+
+
+
+
+
+    }
+
+#    $carhtml=~s/\n//g;
+    $carhtml=~s/\r//g;
+    $carhtml=~s/&nbsp;/ /g;
+    while($carhtml=~/<div class="AdData"[^>]*?>\n\s*<div class="AdDataLeft">(.+?)<\/div>\n\s*<div class="AdDataRight">(.+?)<\/div>\n\s*\n?\s*<\/div>/g) {
+#	print "$1 $2\n";
+	$cardatakey=$1;
+	$cardatavalue=$2;
+	$cardatakey=~s/://g;
+	$cardatakey=~s/č/c/g;
+	$cardatakey=~s/Č/C/g;
+	$cardatakey=~s/Š/S/g;
+	$cardatakey=~s/š/s/g;
+	$cardatakey=~s/Ž/Z/g;
+	$cardatakey=~s/ž/z/g;
+
+	$cardatavalue=~s/č/c/g;
+	$cardatavalue=~s/Č/C/g;
+	$cardatavalue=~s/Š/S/g;
+	$cardatavalue=~s/š/s/g;
+	$cardatavalue=~s/Ž/Z/g;
+	$cardatavalue=~s/ž/z/g;
+
+	$cardata{$cardatakey}=$cardatavalue;
+    }
+
+
+#lokacija
+#    print $carhtml;
+    if($carhtml=~/graphics\/slo(..)\.gif/s) {#
+	$lokacija=$1;
+#	print "lokacija $lokacija\n";
+	$cardata{Lokacija}=$lokacija;
+   }
+
+
+
+
+    $imgurl="http://images.avto.net/$car1id/1.jpg";
+
+    if($carhtml=~/$imgurl/) {
+
+	$cardata{Image1}=$imgurl;
+	$cardata{Image1html}="<img src=\"$imgurl\">";
+
+#	print "Ima sliko";
+    }
+
+
+
+
+
+
+#    print Dumper(\%cardata);
+
+    return %cardata;
+
+
+
+
+}
+
+
+
+
+
+sub gettop100 {
+
+#print "get top 100\n";    
+
+$top100url="http://www.avto.net/_AVTO/results_100.asp";
+
+$top100html=get $top100url;
+
+
+@ids=$top100html=~/ad\.asp\?id=(\d+)>/g;
+
+@ids=uniq(@ids);
+@ids=sort(@ids);
+#print Dumper(@ids);
+
+
+return @ids;
+
+
+}
+
+
+sub uniq {
+    return keys %{{ map { $_ => 1 } @_ }};
+}
+
+
+sub sendemail {
+    my $cardata=shift;
+    my $email = shift;
+    my $msg = MIME::Lite->new(
+        From    => 'Avto.net tracker <lilly@mulaz.org',
+	To      => $email,
+	Subject => "$cardata{Ime} - $cardata{Letnik} letnik - $cardata{Cena} EUR",
+	Type    => 'multipart/mixed'
+	);	
+    
+    $msg->attach(
+	Type     => 'TEXT',
+	Data     => "$cardata{URL}\n\n\n" .Dumper(\%cardata)
+	);
+
+
+
+#zgenerira screesnhot celega oglasa http://code.google.com/p/wkhtmltopdf/issues/list?q=label:wkhtmltoimage
+#ce hoces headless, vzemi binary z linka
+#ce ne rabis zakomentiraj
+    if ($cardata{ID}=~/^\d+$/) {
+	$filename="tmp/".$cardata{ID}.".jpg";
+	`wkhtmltoimage --quality 40 $cardata{URL} $filename 2>/dev/null`;
+
+    }
+#zakomentiraj do tukaj ce ne rabis    
+    
+    
+    
+    $msg->attach(
+	Type     => 'image/jpeg',
+     	Filename => $id.".1.jpg",
+	Data => get($cardata{Image1}),
+     	);
+    
+
+#attacha se screenshot pagea, ce ne rabis, zakomentiraj
+    $msg->attach(
+        Type     => 'image/jpeg',
+        Filename => $id.".jpg",
+	Path => $filename,
+        );
+#do tukaj
+
+
+
+
+    $msg->send();
+    
+    
+
+
+}
